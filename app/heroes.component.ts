473b8550692698927f829a939c1ec8f6e4ec9d44
Varunendra Pratap Singh <vpsingh016@gmail.com>
2016-06-18 02:23:13 +0530

    Http

--- app/heroes.component.ts
+++ app/heroes.component.ts
@@ -9,17 +9,22 @@ import { HeroService } from './hero.service';
 @Component({
 	selector: 'my-heroes',
 	styleUrls:  ['app/heroes.component.css'],
-	templateUrl: 'app/heroes.component.html'
+	templateUrl: 'app/heroes.component.html',
+	directives: [HeroDetailComponent]
 })
 
 export class HeroesComponent implements OnInit {
 	heroes: Hero[];
 	selectedHero: Hero;
+	addingHero = false;
+	error: any;
 
 	constructor(private router: Router, private heroService: HeroService) {}
 
 	getHeroes() {
-	    this.heroService.getHeroes().then(heroes => this.heroes = heroes);
+	    this.heroService.getHeroes()
+	    	.then(heroes => this.heroes = heroes)
+	    	.catch(error => this.error = error); // TODO: Display error message
 	}
 
 	ngOnInit() {
@@ -32,5 +37,26 @@ export class HeroesComponent implements OnInit {
 
 	gotoDetail() {
 	    this.router.navigate(['HeroDetail', { id: this.selectedHero.id }]);
+	}
+
+	addHero() {
+	  this.addingHero = true;
+	  this.selectedHero = null;
+	}
+
+	close(savedHero: Hero) {
+	  this.addingHero = false;
+	  if (savedHero) { this.getHeroes(); }
+	}
+
+	delete(hero: Hero, event: any) {
+	  event.stopPropagation();
+	  this.heroService
+	      .delete(hero)
+	      .then(res => {
+	        this.heroes = this.heroes.filter(h => h !== hero);
+	        if (this.selectedHero === hero) { this.selectedHero = null; }
+	      })
+	      .catch(error => this.error = error); // TODO: Display error message
 	}
 }
